eae8213b470e1f5770cb4b90ffd5f1cc87bbca1d
akame-robot <wolfinfiresnow@gmail.com>
2023-07-27 18:48:37 -0400

    commuit

---
+++ Teste Banco/TesteAula.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http.Headers;
+using Teste_Banco;
+using Teste_Banco.Script;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class TesteAula
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
