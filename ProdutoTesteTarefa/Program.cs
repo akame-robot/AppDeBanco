1a4772724be7b12642c49e802aca70d2adb4a9e9
akame-robot <wolfinfiresnow@gmail.com>
2023-07-27 21:19:01 -0400

    ck

--- ProdutoTesteTarefa/Program.cs
+++ ProdutoTesteTarefa/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProdutoTesteTarefa;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Product> list = new List<Product>();
+            Console.Write("numero de produtos: ");
+            int n = int.Parse(Console.ReadLine());
+           for(int i = 0; i <= n; i++)
+            {
+                Console.WriteLine($"Produto #{i} info: ");
+                Console.Write("comum, usado ou importado (c/u/i)?: ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = int.Parse(Console.ReadLine());
+                if(type == 'c')
+                {
+                    list.Add(new Product(name, price));
+
+                }
+                else if (type == 'u')
+                {
+                    Console.Write("data de fabricação (dd/mm/yyy): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+
+
+                }
+                else
+                {
+                    Console.Write("taxa da alfandega: ");
+                    double customFee = double.Parse(Console.ReadLine());
+                    list.Add(new ImportedProduct(name, price, customFee));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine($"etiqueta de preço:");
+                foreach(Product product in list)
+                {
+                    Console.WriteLine(product.PriceTag());
+                }
+
+            }
+        }
+    }
+}
---
+++ ProdutoTesteTarefa/UsedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutoTesteTarefa
+{
+    internal class UsedProduct :Product
+    {
+
+        public DateTime ManufacturyDate { get; set; }
+
+        public UsedProduct()
+        {
+
+        }
+        public UsedProduct(string name, double price, DateTime manufacturyDate) : base (name, price)
+        {
+            ManufacturyDate = manufacturyDate;
+        }
+        public override string PriceTag()
+        {
+            return Name + "(Usado) $" + Price.ToString("F2") + "(Data de fabricaçao" + ManufacturyDate.ToString("dd/mm/yyyy") + ")";
+        }
+    }
+}
